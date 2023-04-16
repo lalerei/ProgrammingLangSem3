a8c8f4835f17338c0130e12582f09f673f5a7dff
lalerei <lale.ley@gmail.com>
2023-04-16 13:29:38 +0300

    completed

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая по заданному номеру четверти, 
 // показывает диапазон возможных координат точек в этой четверти (x и y).
-/*
+
 void ShowRange(int quad)
 {
   if(quad ==1) Console.WriteLine("x > 0 and y > 0");
@@ -14,17 +14,18 @@ Console.Write("Input a number of quadrant: ");
 int quadrant = Convert.ToInt32(Console.ReadLine());
 
 ShowRange(quadrant);
-*/
+
 
 // Напишите программу, которая принимает на вход координаты точки (X и Y), 
 // и выдаёт номер четверти плоскости, в которой находится эта точка.
 
-int returnRes(double x, double y)
+int returnCoord(double x, double y)
 {
   if (x > 0 && y > 0) return 1;
   else if (x < 0 && y > 0) return 2;
   else if (x < 0 && y < 0) return 3;
   else if (x > 0 && y < 0) return 4;
+  else return 0;
 }
 
 Console.WriteLine("Enter coord x: ");
@@ -32,10 +33,10 @@ double coordX = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Enter coord y: ");
 double coordY = Convert.ToDouble(Console.ReadLine());
 
-double returnRes = returnRes(coordX, coordY);
+double returnRes = returnCoord(coordX, coordY);
 Console.WriteLine($"Ваша координатная плоскость {returnRes}");
 
-/*
+
 // Напишите программу, которая принимает на вход число (N) 
 // и выдаёт таблицу квадратов чисел от 1 до N. (e.g. 1-1 2-4 3-9 4-16)
 
@@ -51,11 +52,11 @@ void TablKvad (int N)
 Console.Write ("Введите значение  - ");
 int num = Convert.ToInt32(Console.ReadLine()); 
 TablKvad (num);
-*/
+
 
 // Напишите программу, которая принимает на вход координаты двух точек и 
 // находит расстояние между ними в 2D пространстве.
-/*
+
 double Gipot (double x1, double y1, double x2, double y2)
 {
   double distX = x2-x1;
@@ -77,8 +78,3 @@ int y2 = Convert.ToInt32(Console.ReadLine());
 
 double rez = Math.Round(Gipot(x1,y1,x2,y2),4);
 Console.Write ($"Расстояние между точками 1 и 2  составляет  - {rez}");
-// Console.Write ($"Расстояние между точками 1 и 2  составляет  - {Gipot (x1,y1,x2,y2)}");
-// double rez = Math.Round(Gipot(x1,y1,x2,y2),4);
-*/
-
-// https://github.com/lalerei/ProgrammingLangSem3.git
